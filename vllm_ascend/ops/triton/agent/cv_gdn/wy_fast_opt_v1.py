ef181ccfaba92c365f8a1e6268bfb2efbb7d87e0
Fager10086 <77871921+Fager10086@users.noreply.github.com>
2026-04-20 15:22:22 +0800

    Create wy_fast_opt_v1.py
    Signed-off-by: Fager10086 <77871921+Fager10086@users.noreply.github.com>

---
+++ vllm_ascend/ops/triton/agent/cv_gdn/wy_fast_opt_v1.py
@@ -0,0 +1,162 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+# SPDX-FileCopyrightText: Songlin Yang, Yu Zhang
+#
+# This file contains code copied from the flash-linear-attention project.
+# The original source code was licensed under the MIT license and included
+# the following copyright notice:
+# Copyright (c) 2023-2025, Songlin Yang, Yu Zhang
+
+# ruff: noqa: E501
+# mypy: ignore-errors
+
+import torch
+import triton.runtime.driver as driver
+from vllm.triton_utils import tl, triton
+
+from .utils import prepare_chunk_indices
+
+device = torch.npu.current_device()
+properties = driver.active.utils.get_device_properties(device)
+AICORE_NUM = properties["num_aicore"]
+AIVECCORE_NUM = properties["num_vectorcore"]
+
+
+@triton.heuristics({"IS_VARLEN": lambda args: args["cu_seqlens"] is not None})
+@triton.jit(do_not_specialize=["T", "B"])
+def recompute_w_u_fwd_kernel(
+    k,
+    v,
+    beta,  # [H, B, T]
+    w,
+    u,
+    A,
+    g,  # [H, B, T]
+    cu_seqlens,
+    chunk_indices,
+    T,
+    B,
+    H: tl.constexpr,
+    Hg: tl.constexpr,
+    K: tl.constexpr,
+    V: tl.constexpr,
+    BT: tl.constexpr,
+    BK: tl.constexpr,
+    BV: tl.constexpr,
+    IS_VARLEN: tl.constexpr,
+    grid0=0,
+):
+    core_num = tl.num_programs(0)
+    pid = tl.program_id(0)
+    T_orig = T
+
+    for i_t_o in range(pid, grid0, core_num):
+        T = T_orig
+        bt_stride = B * T
+
+        for i_bh in range(B * H):
+            i_b, i_h = i_bh // H, i_bh % H
+            if IS_VARLEN:
+                i_n, i_t = (
+                    tl.load(chunk_indices + i_t_o * 2).to(tl.int32),
+                    tl.load(chunk_indices + i_t_o * 2 + 1).to(tl.int32),
+                )
+                bos, eos = tl.load(cu_seqlens + i_n).to(tl.int32), tl.load(cu_seqlens + i_n + 1).to(tl.int32)
+                T = eos - bos
+            else:
+                bos, eos = i_b * T, i_b * T + T
+                i_t = i_t_o
+
+            offs_t = tl.arange(0, BT)
+            global_offs_t = i_t * BT + offs_t
+            mask_t = global_offs_t < T
+
+            offs_t_2d = global_offs_t[:, None]
+            offs_bt = tl.arange(0, BT)[None, :]
+            ptr_A = A + (bos * H + i_h) * BT + offs_t_2d * (H * BT) + offs_bt * 1
+            mask_A = mask_t[:, None]
+            b_A = tl.load(ptr_A, mask=mask_A, other=0.0).to(tl.float32)
+
+            ptr_g = g + i_h * bt_stride + bos + global_offs_t
+            b_g = tl.exp(tl.load(ptr_g, mask=mask_t, other=0.0)).to(tl.float32)
+
+            ptr_beta = beta + i_h * bt_stride + bos + global_offs_t
+            b_beta = tl.load(ptr_beta, mask=mask_t, other=0.0).to(tl.float32)
+
+            for i_v in range(tl.cdiv(V, BV)):
+                offs_v = i_v * BV + tl.arange(0, BV)[None, :]
+                mask_v = (mask_t[:, None]) & (offs_v < V)
+
+                ptr_v = v + (bos * H + i_h) * V + offs_t_2d * (H * V) + offs_v * 1
+                b_v = tl.load(ptr_v, mask=mask_v, other=0.0).to(tl.float32)
+
+                b_vb = b_v * b_beta[:, None]
+                b_u = tl.dot(b_A, b_vb, allow_tf32=False)
+
+                ptr_u = u + (bos * H + i_h) * V + offs_t_2d * (H * V) + offs_v * 1
+                tl.store(ptr_u, b_u.to(ptr_u.dtype.element_ty), mask=mask_v)
+
+            for i_k in range(tl.cdiv(K, BK)):
+                offs_k = i_k * BK + tl.arange(0, BK)[None, :]
+                mask_k = (mask_t[:, None]) & (offs_k < K)
+                ptr_k = k + (bos * Hg + i_h // (H // Hg)) * K + offs_t_2d * (Hg * K) + offs_k * 1
+                b_k = tl.load(ptr_k, mask=mask_k, other=0.0).to(tl.float32)
+
+                b_kb = b_k * b_beta[:, None] * b_g[:, None]
+                b_w = tl.dot(b_A, b_kb)
+
+                ptr_w = w + (bos * H + i_h) * K + offs_t_2d * (H * K) + offs_k * 1
+                tl.store(ptr_w, b_w.to(ptr_w.dtype.element_ty), mask=mask_k)
+
+
+def recompute_w_u_fwd(
+    k: torch.Tensor,
+    v: torch.Tensor,
+    beta: torch.Tensor,
+    g_cumsum: torch.Tensor,
+    A: torch.Tensor,
+    cu_seqlens: torch.LongTensor | None = None,
+    chunk_indices: torch.Tensor | None = None,
+) -> tuple[torch.Tensor, torch.Tensor]:
+    B, T, Hg, K, V = *k.shape, v.shape[-1]
+    H = v.shape[-2]
+    BT = A.shape[-1]
+
+    if cu_seqlens is not None and chunk_indices is None:
+        chunk_indices = prepare_chunk_indices(cu_seqlens, BT)
+    NT = triton.cdiv(T, BT) if cu_seqlens is None else len(chunk_indices)
+
+    BK = 64
+    BV = 64
+
+    u = torch.empty_like(v)
+    w = k.new_empty(B, T, H, K)
+    beta = torch.permute(beta, (2, 0, 1)).contiguous()
+    g_cumsum = torch.permute(g_cumsum, (2, 0, 1)).contiguous()
+
+    core_num = AICORE_NUM
+    grid = (core_num,)
+    recompute_w_u_fwd_kernel[grid](
+        k=k,
+        v=v,
+        beta=beta,
+        w=w,
+        u=u,
+        A=A,
+        g=g_cumsum,
+        cu_seqlens=cu_seqlens,
+        chunk_indices=chunk_indices,
+        T=T,
+        B=B,
+        H=H,
+        Hg=Hg,
+        K=K,
+        V=V,
+        BT=BT,
+        BK=BK,
+        BV=BV,
+        grid0=NT,
+        num_warps=4,
+        num_stages=3,
+    )
+    return w, u
